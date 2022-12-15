b883f0a56d3259f5b6ce3dca027939663980f979
leosmaia21 <leosmaia21@gmail.com>
2022-12-15 18:48:22 +0000

    ja nao é preciso a foto intermediaria ao fazer inferencia

--- yolov7/yolo_processing.py
+++ yolov7/yolo_processing.py
@@ -1,11 +1,11 @@
 import glob
 import sys
 
-sys.path.append("yolov7")
-from torch._jit_internal import loader
-from torchvision.transforms import transforms
+import cv2
 
-from yolov7.yolov7.utils.datasets import LoadImages
+sys.path.append("yolov7")
+
+from yolov7.utils.datasets import LoadImages, letterbox
 
 import os
 from pathlib import Path
@@ -13,9 +13,8 @@ import PIL
 from PIL import Image
 import numpy as np
 import torch
-import cv2
 from yolov7.models.experimental import attempt_load
-from yolov7.yolov7.utils.general import non_max_suppression, scale_coords, xyxy2xywh
+from yolov7.utils.general import non_max_suppression, scale_coords, xyxy2xywh
 import torch
 import torch.backends.cudnn as cudnn
 
@@ -29,7 +28,7 @@ def detectYolo(step):
 
     # half = device.type != 'cpu'  # half precision only supported on CUDA
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda")
     # Load model
     model = attempt_load(weights[0], map_location=device)
 
@@ -62,48 +61,35 @@ def detectYolo(step):
     #         xmax = dim
     #         ymin = ymax
     #         ymax = ymax + dim
-    print("finish")
     x = []
     y = []
-    img_test = Image.open('teste.tif')
-    # img_test = img_test.convert('RGB')
-    # img_test = np.array(img_test)
-    # img_test = img_test.transpose(2,0,1)
-    # if (np.mean(numpyarray)) != 0:
-    img_test = LoadImages('teste.tif')
-    for path, im, im0s, vid_cap in img_test:
-        im = im
-        im = torch.from_numpy(im).to(device)
-        im = im.float()  # uint8 to fp16/32
-        im /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if im.ndimension() == 3:
-            im = im.unsqueeze(0)
-        # print(im)
-        model.eval()
-        pred = model(im)[0]
-        pred = non_max_suppression(pred)
-        for i, det in enumerate(pred):  # detections per image
-            p, im0, frame = path, im0s.copy(), getattr(img_test, 'frame', 0)
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+    img0 = Image.open('teste.tif')
+    img0 = cv2.cvtColor(np.array(img0), cv2.COLOR_RGB2BGR)
+    img = letterbox(img0)[0]
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+    img = torch.from_numpy(img).to(device)
+    img = img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    # print(im)
+    model.eval()
+    pred = model(img)[0]
+    pred = non_max_suppression(pred)
+    for i, det in enumerate(pred):  # detections per image
+        im0, frame = img0.copy(), getattr(img0, 'frame', 0)
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    x.append(xywh[0] * im.shape[2])
-                    y.append(xywh[1] * im.shape[2])
-    print(x, ", ",y)
-    #     if len(im.shape) == 3:
-    #         im = im[None]  # expand for batch dim
-    #
-    #     n, X, Y = resultYOLO(model(im), model, path, im, im0s, img, s)
-    #     print("n", n, "x", X, "y", Y)
-    #     image = cv2.imread("teste2.tif")
-    #     for i in range(n):
-    #         image = cv2.circle(image, (int(X[i]), int(Y[i])), 15, (255, 0, 0))
-    #     cv2.imwrite("a.jpg", image)
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                x.append(xywh[0] * img.shape[2])
+                y.append(xywh[1] * img.shape[2])
+    print(x, ", ", y)
 
 
 detectYolo(100)
